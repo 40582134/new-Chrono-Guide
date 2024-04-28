9d408838cb6c37db63270d3f2529bc5460a156a8
40582134 <124286253+40582134@users.noreply.github.com>
2024-04-28 21:11:03 +0100

    Update vite.config.js
    updated to match target url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/new-Chrono-Guide/",
+  base: "/Chrono-Guide-Vercel-Patch-P/",
   build: {
     outDir: "docs",
   },
